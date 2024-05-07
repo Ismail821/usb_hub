43ba64817107f16b459913fb54c4898f5a38bfa3
praneet-02 <praneetmugu@gmail.com>
2024-05-07 18:55:47 +0530

    sequence_detector made dynamic, added seq_det input, this seq will be detected

--- rtl/sequence_detector.v
+++ rtl/sequence_detector.v
@@ -2,22 +2,22 @@
 //sequence detector for nak
 //
 
-
 module sequence_detector(
 		input wire clk,
 		input wire rst,
 		input wire serial_data_in,
 		input wire serial_data_in_valid,
-
+		input wire [7:0] seq_det, 
+		
 		output reg sequence_detected
 
     );
+    
+reg [2:0]state;       
 
-reg [2:0]state;
+reg [7:0] seq;
 
-reg [7:0] seq = 8'b01011010; //PID for NAK
-
-reg deb;
+reg seq_flag;
 
 parameter IDLE = 3'b000;
 parameter A = 3'b001;
@@ -29,28 +29,29 @@ parameter F = 3'b110;
 parameter G = 3'b111;
 
 
-always@(posedge clk)begin
-
+always@(posedge clk)begin 
+	
+	seq = seq_det;
 	if(rst)begin
 		state<=IDLE;
 		sequence_detected<=0;
-		deb<=0;
+		seq_flag<=0;
+	
 	end
   else if(serial_data_in_valid) begin
-  	deb<=1;
     case(state)
-
+    	
     	IDLE:begin
-		sequence_detected<=0;
+				sequence_detected<=0;    	
     		if(serial_data_in==seq[0])begin
     			state<=A;
     		end
     		else begin
     			state<=IDLE;
-
+    			
     		end
     	end
-
+    	
     	A: begin
     		if(serial_data_in==seq[1])begin
     			state<=B;
@@ -74,7 +75,7 @@ always@(posedge clk)begin
     			state<=IDLE;
     		end
     	end
-
+    	
     	C: begin
     		if(serial_data_in==seq[3])begin
     			state<=D;
@@ -86,71 +87,75 @@ always@(posedge clk)begin
     			state<=IDLE;
     		end
     	end
-
+    	
      	D: begin
     		if(serial_data_in==seq[4])begin
-			state<=E;
-		end
-		else if(serial_data_in==seq[0]) begin
-			state<=A;
-		end
-		else begin
-	 		state<=IDLE;
-		end
+					state<=E;
+				end
+				else if(serial_data_in==seq[0]) begin
+					state<=A;
+				end
+				else begin
+					state<=IDLE;
+				end
     	end
-
+    	
     	E: begin
     		if(serial_data_in==seq[5])begin
-			state<=F;
-		end
-		else if(serial_data_in==seq[0]) begin
-			state<=A;
-		end
-		else begin
-			state<=IDLE;
-		end
+					state<=F;
+				end
+				else if(serial_data_in==seq[0]) begin
+					state<=A;
+				end
+				else begin
+					state<=IDLE;
+				end
     	end
-
+    	
     	F: begin
     		if(serial_data_in==seq[6])begin
-			state<=G;
-		end
-		else if(serial_data_in==seq[0]) begin
-			state<=A;
-		end
-		else begin
-			state<=IDLE;
-		end
-    	end
-
+					state<=G;
+				end
+				else if(serial_data_in==seq[0]) begin
+					state<=A;
+				end
+				else begin
+					state<=IDLE;
+				end
+    	end    	    
+    	
     	G: begin
     		if(serial_data_in==seq[7])begin
-			state<=IDLE;
-			sequence_detected<=1;
-		end
-		else if(serial_data_in==seq[0]) begin
-			state<=A;
-		end
-		else begin
-			state<=IDLE;
-		end
-    	end
-
+					state<=IDLE;
+					sequence_detected<=1;
+					seq_flag<=0;
+				end
+				else if(serial_data_in==seq[0]) begin
+					state<=A;
+				end
+				else begin
+					state<=IDLE;
+				end
+    	end    		   	    	    	
+    	
     	default:begin
     		state<=IDLE;
     		sequence_detected<=0;
     	end
-
-
+    	
+    	
     endcase
-
+    
   end
   else begin
   	sequence_detected<=0;
   	state<=IDLE;
-  end
+  	seq_flag<=0;
+  end 
+  
+  
 
-end
-
+   
+end    
+    
 endmodule
-
